56096f4f4c739f19c569542705ecde8ecd8f1d57
Ana Zeneli <az2498@columbia.edu>
2018-01-17 00:29:40 -0500

    Website template and redirect

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
+# user = 'az2498'
 
 @app.route('/', methods=['GET'])
-def main():
+def index():
     return render_template('index.html')
+
+@app.route('/az2498', methods=['GET'])
+def about_me():
+    return render_template('aboutme.html')
 
 
 if __name__ == '__main__':
